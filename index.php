104884294d897064154bb9c2977fcc8fcb14236b
jarusking <jarusp29@users.noreply.github.com>
2019-06-28 22:32:02 +0300

    update
    fixes

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <?php
 $to="jarusp29@gmail.com";
-$subject="Resposce from Website";
-$message="I'm Very thankful to you";
-$headers="jarusp29@users.noreply.github.com";
+$subject="Response from website";
+$message="I'm very thankful";
+$headers="From: jarusp29@users.noreply.github.com";
 if (mail($to, $subject, $message, $headers)){
-
-	echo "Mail sent successfuly";
-
+	echo "Mail sent successfully";
 }
 else{
-	echo "Mail not sent";
+	echo "Mail cannot sent";
 }
-
-
 
 ?>
